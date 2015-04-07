b1bb3e7a46618bc8b1f2e8eb21d524d25318608b
ccnezin <ccnezin@gmail.com>
2015-04-06 21:51:48 -0400

    bla
    fixed

--- main2.asm
+++ main2.asm
@@ -1,10 +1,6 @@
 .data
-file: .asciiz "bookss.txt"      # Name of the file in the //same directory//
-dontask: .space 1
-textbuffer: .space 0x4FFFF 	#Creates a space for the text
-buffer: .space 0xFFFF
-
-
+textbuffer: .space 0x4FFF0 	#Creates a space for the text
+wordbuffer: .space 0x4FFF0
 
 .text
 
@@ -20,10 +16,10 @@ syscall      # Puts the file descriptor in $v0
 move $a0, $v0
 li $v0, 14    #Prepares file to be read
 la $a1, textbuffer      #Specifies buffer
-li $a2, 0x4FFFF      #Specifies length
+li $a2, 0x4FFF0      #Specifies length
 syscall      # Reads it.
 
-li $t0, 0x000000FF
+li $t0, 0x000000FF	#For isolating bits
 li $t1, 0x00000040 	#one less than A
 li $t2, 0x0000005B	#One more than Z
 li $t3, 0x00000060	#One less than a
@@ -33,6 +29,8 @@ li $t6, 0x00000021	#?
 li $t7, 0x0000002E	#.
 li $t8, 0x0000003F	#!
 li $t9, 0x0000000A	#\n
+
+la $s6, wordbuffer
 
 add $v0, $v0, $a1
 
@@ -48,8 +46,6 @@ and $s1, $s1, $t0
 jal lettertest
 jal punctest
 jal spacetest
-
-beq $s1, $zero, done
 
 addi $s8, $s8 8
 beq $s8, 32, nextbyte
@@ -74,6 +70,10 @@ jr $ra
 letteradd:
 addi $s2, $s2, 1
 addi $s7, $zero, 1
+
+sb $s1, 0($s6)
+addi $s6, $s6, 1
+
 jr $ra
 
 ############################
@@ -87,7 +87,8 @@ jr $ra
 spaceadd:
 addi $s3, $s3, 1
 addi $s7, $zero, 0
-jr $ra
+j checkifunique
+
 
 ############################
 
@@ -96,15 +97,74 @@ beq $s7, $zero, jumpback
 beq $s1, $t6, puncadd
 beq $s1, $t7, puncadd
 beq $s1, $t8, puncadd
-
 jr $ra
 
 puncadd:
 addi $s4, $s4, 1
 addi $s7, $zero, 0
-jr $ra
+j checkifunique
 
 jumpback:
+jr $ra
+
+checkifunique:
+li $a2, 0x10
+div $s6, $a2
+mfhi $a2
+sub $a2 $s6, $a2
+la $t0, wordbuffer
+
+loop:
+addi $t0, $t0, 0x10
+lw $t1, 0($t0)
+beq $t1, $zero, isunique
+lw $t5, 0($a2)
+bne $t1, $t5, loop
+bne $t2, $t6, loop
+bne $t3, $t7, loop
+bne $t4, $t8, loop
+j notunique
+
+isunique:
+li $a2, 0x10
+div $s6, $a2
+mfhi $a2
+li $t9, 16
+sub $a2, $t9, $a2
+add $s6, $s6, $a2
+
+li $t0, 0x000000FF	#For isolating bits
+li $t1, 0x00000040 	#one less than A
+li $t2, 0x0000005B	#One more than Z
+li $t3, 0x00000060	#One less than a
+li $t4, 0x0000007B	#One more than z
+li $t5, 0x00000020	#space
+li $t6, 0x00000021	#?
+li $t7, 0x0000002E	#.
+li $t8, 0x0000003F	#!
+li $t9, 0x0000000A	#\n
+
+jr $ra
+
+notunique:
+
+addi $s6, $a2, 0
+sw $zero, 0($a2)
+sw $zero, 16($a2)
+sw $zero, 32($a2)
+sw $zero, 48($a2)
+
+li $t0, 0x000000FF	#For isolating bits
+li $t1, 0x00000040 	#one less than A
+li $t2, 0x0000005B	#One more than Z
+li $t3, 0x00000060	#One less than a
+li $t4, 0x0000007B	#One more than z
+li $t5, 0x00000020	#space
+li $t6, 0x00000021	#?
+li $t7, 0x0000002E	#.
+li $t8, 0x0000003F	#!
+li $t9, 0x0000000A	#\n
+
 jr $ra
 
 done:
@@ -125,5 +185,6 @@ cvt.s.w $f2, $f2
 
 div.s $f4, $f0, $f2
 div.s $f5, $f3, $f0
+
 
 
