792b0d3386fb6049ea012712001cc28079eb1cd6
ccnezin <ccnezin@gmail.com>
2015-04-17 13:37:44 -0400

    Nearly working
    This code has the addition of the data base reading.  It currently has
    code which can read in the arguments as well as the author name.  It
    then calculates a difference-geometric-mean between the two linguistic
    features as a way of measuring "how close" the author is to the text.

--- backup.asm
+++ backup.asm
@@ -1,15 +1,18 @@
 #This program is written by Cory Nezin and Ross Kaplan
-#Most recently edited 4/7/2015
-#The purpose of this code is to determine authorship through analysis of linguistic features.
+#Most recently edited 4/13/2015
+#The purpose of this code is to determine authorship through analysis of linguistic
+#features such as average words per sentence, average characters per word, and percentage of unique words
 
 .data
-file: .space 0xFF
-null: .asciiz ""
-textbuffer: .space 0x4FFF0 	#Creates a space for the text
-databuffer: .space 0xFF00
-datafile: .asciiz "database.txt"
-buffer: .space 3
-wordbuffer: .space 0x4FFF0
+file: .space 0xFF	#This space is reserved for the user inputted file
+null: .asciiz ""	#This is to make sure it gets null terminated
+textbuffer: .space 0x4FFF0 	#Creates a space for the author's text
+databuffer: .space 0xFF00	#Creates a space for the database
+datafile: .asciiz "database.txt"	#Name of the database file
+buffer: .space 3	#to allign the next memory
+wordbuffer: .space 0x4FFF0	#Creates a space for the "dictionary"
+currentauthor: .space 0x1000
+bestauthor: .space 0x1000
 
 
 .text
@@ -18,20 +21,20 @@ wordbuffer: .space 0x4FFF0
 main:
 
 li $v0, 8 #read string
-la $a0, file
-li $a1, 0x100
+la $a0, file	#Gets the addres of the name of the author's text - to be inputted by user
+li $a1, 0x100	#Max possible length of the input
 syscall
 
 #all of the following code before "open" is to replace the newline from console with a null.
 
-addi $t3, $zero, 0x0a
-add $t1, $zero, $a0
+addi $t3, $zero, 0x0a	#t3 is the newline which shows up from the console input
+add $t1, $zero, $a0		#sets t1 equal to the starting address of the file name
 
-nullify:
+nullify:	#This block searches the code for a newline
 lw $t0, 0($t1)
 
-andi $t2, $t0, 0xFF
-beq, $t2, $t3, loadnull1
+andi $t2, $t0, 0xFF	#Each of these four is a check on each byte of the word
+beq, $t2, $t3, loadnull1	#If it finds a newline, call a subroutine which replaces it with a null
 srl $t0, $t0, 8
 andi $t2, $t0, 0xFF
 beq, $t2, $t3, loadnull2
@@ -42,14 +45,14 @@ srl $t0, $t0, 8
 andi $t2, $t0, 0xFF
 beq, $t2, $t3, loadnull4
 addi $t1, $t1, 4
-j nullify
+j nullify	#go to the next word (mips word) if there's no newline
 
-loadnull1:
-lw $t0, 0($t1)
-li $t4, 0xFFFFFF00
+loadnull1:	#Each of these will replace the newline with a null - note that it matters which byte of the word it is in so there must be four cases
+lw $t0, 0($t1)	#Go to the word which we found the newline in
+li $t4, 0xFFFFFF00	#If this loadnull was called then this is the correct pattern for replacement (we don't want to effect the other bytes in the word)
 and $t0, $t0, $t4
-sw $t0, 0($t1)
-j open
+sw $t0, 0($t1)	#Overwrite the old word with the null terminated word
+j open	#Start the actual program
 loadnull2:
 lw $t0, 0($t1)
 li $t4, 0xFFFF0000
@@ -71,23 +74,17 @@ j open
 
 open:
 
-####### These two syscalls open the database
-li $v0, 13      # Prepare to open file.
-la $a0, datafile
-li $a1, 0		#unused
-li $a2, 0		#unused
-syscall      # Puts the file descriptor in $v0
-
-move $a0, $v0
-
-li $v0, 14    #Prepares file to be read
-la $a1, databuffer      #Specifies buffer
-li $a2, 0xFF00      #Specifies length
-syscall      # Reads it.
-la $t1, databuffer
-li $t0, 0xFF00	#Goes to the end of databuffer
-add $t1, $t1, $t0
-sw $v0, 0($t1)		#Stores the length of the database there for LATER
+#s0 - none
+#s1 - The current character being read
+#s2 - The total amount of letters read so far
+#s3 - The total amount of spaces and newlines
+#s4 - The total amount of punctuation - ! . ?
+#s5 - The total amount of repeated words
+#s6 - The current location in the dictionary
+#s7 - If this is zero the most recent character read was a space of punctuation.
+#t9 - used as a permenant register, represents the state of there being a digit in the current word
+#v0 - used as a permenant register, represents the location of the end of the file
+#a0 - used as a permenant register, represents the current location in the file
 
 ####### These two syscalls open the book
 li $v0, 13      # Prepare to open file.
@@ -102,23 +99,28 @@ la $a1, textbuffer      #Specifies buffer
 li $a2, 0x4FFF0      #Specifies length
 syscall      # Reads it.
 
+#This makes v0 the length of the file
 
 
+addi $t9, $zero, 0	#initialize to 0, just in case
+la $s6, wordbuffer	#Start of "dictionary
 
-addi $t9, $zero, 0	#initialize to 0, probably not necessary
-la $s6, wordbuffer	#Start of dictionary
+add $v0, $v0, $a1	#v0 now represents the end of the file
 
-add $v0, $v0, $a1	#End of file
+#Note that the following code for extracting characters may look odd.  Instead of reading the memory byte by byte
+#we load in a word at a time and extract each byte individually.  This is mainly an experiment in optimization -
+#While it is probably not more optimal in SPIM it may have a faster run time in actual MIPS because memory access
+#is one of the largest time wasters, this method calls memory only 1/4th of the amount that would be called doing it byte by byte.
 
-nextword:
-lw $s0, 0($a1)	#Load the 4 characters
-addi $a1, $a1, 4	#Next loop uses 4 characters
-addi $s8, $zero, 0	#reset s8
+nextword:	#This loop loads the next 'mips' word in the author's text
+lw $s0, 0($a1)	#Load the 4 characters into s0
+addi $a1, $a1, 4	#Next loop uses the next 4 characters
+addi $s8, $zero, 0	#reset s8, s8 is used as a number which determines what character of the word we are looking at
 bgt $a1, $v0, done	#Stop when you get to the end of the file
 
 nextchar:
 srlv $s1, $s0, $s8	#put the next character in s1
-andi $s1, $s1, 0xFF	#isolate it
+andi $s1, $s1, 0xFF	#isolate it and store it in s1
 
 jal lettertestLOWER	#Subroutine which determines if the character is a letter (input is s1)
 jal punctest	#Subroutine which determines if the character is punctuation (input is s1)
@@ -126,7 +128,7 @@ jal spacetest	#Subroutine which determines if the character is a space or newlin
 jal numbertest	#Subroutine which determines if the character is a digit (input is s1)
 
 skip:	#skip the rest of the tests if the character has been determined
-addi $s8, $s8 8		#so the next shift will provide the correct character
+addi $s8, $s8, 8		#so the next shift will provide the correct character
 beq $s8, 32, nextword	#Once you've read 4 characters, go on to the next byte
 j nextchar	#Otherwise go to the next character
 
@@ -166,7 +168,7 @@ sw $zero, 8($a2)
 sw $zero, 12($a2)
 addi $s6, -16
 
-j skip	#No need for other tests on this character
+j skip	#No need for other tests on this character, it is already determined
 
 ###########################
 
@@ -197,13 +199,13 @@ addi $s1, $s1, 32
 j letteradd
 
 letteradd:
-bgt $t9, $zero, skip	#if there is a digit in the word, don't count it (this should probably be moved somewhere further up)
-addi $s2, $s2, 1
+bgt $t9, $zero, skip	#if there is a digit in the word, don't count it (this should probably be moved somewhere further up to increase efficiency but it's not a common case anyway)
+addi $s2, $s2, 1		#s2 is the total amount of letters
 addi $s7, $zero, 1
 
 sb $s1, 0($s6)	#Save the character to the dictionary
-addi $s6, $s6, 1	#s6 is the total letter count for the whole text
-addi $t8,  $t8, 1	#t8 represents the amount of characters in the word SO FAR.  We need to subtract this if we encounter a digit later in the word
+addi $s6, $s6, 1	#s6 is the "location in the text"
+addi $t8,  $t8, 1	#t8 represents the amount of characters in the word SO FAR.  We need to subtract this if we encounter a digit later in the word to undo our mistakes.
 
 j skip
 
@@ -221,9 +223,9 @@ jr $ra
 
 spaceadd:
 addi $s3, $s3, 1
-addi $s7, $zero, 0
-addi $t9, $zero, 0
-j checkifunique
+addi $s7, $zero, 0	#s7 explained in puncadd
+addi $t9, $zero, 0	#t9 explained in puncadd
+j checkifunique		#Now that a word has been finished, we check to see if it is a repeat of a previous word - for our unique word feature/dictionary
 
 
 ############################
@@ -242,9 +244,9 @@ jr $ra
 
 puncadd:
 addi $s4, $s4, 1	#s4 is number of periods
-addi $s7, $zero, 0	#most recent character is space/punc
+addi $s7, $zero, 0	#most recent character is space/punctuation
 addi $t9, $zero, 0	#No number in the current word now
-j checkifunique		#This subroutine checks if the word just loaded is unique
+j checkifunique		
 
 
 checkifunique:
@@ -255,6 +257,8 @@ sub $a2 $s6, $a2	#subtract to get to the beginning of the word so we can compare
 
 la $t0, wordbuffer
 addi $t0, $t0, -16	#So it starts out at 0
+
+#The following basically compares every unique word to the current word being tested
 
 loop:
 addi $t0, $t0, 16
@@ -295,7 +299,7 @@ sw $zero, 8($a2)
 sw $zero, 12($a2)
 
 jr $ra
-
+#The following code is executed when we've read the entire file, it does the basic calculations
 done:
 addi $s4, $s4, 1
 add $s0, $s3, $s4
@@ -319,3 +323,213 @@ cvt.s.w $f6, $f6
 div.s $f4, $f0, $f2 # Average words per sentece
 div.s $f5, $f3, $f0	# Average characters per word
 div.s $f7, $f6, $f0	# Fraction of unique words in the text
+
+#PAST THIS POINT VARIABLES HAVE DIFFERENT MEANINGS
+#s0 - none
+#s1 - The character that was just read in
+#s2 - This number is the amount of digits to the right of the decimal
+#s3 - the current byte in the author's name memory location
+#s4 - The current field in the database - 1 is average words per sentence, 2 is average chars per word, 3 is author name
+#s5 - the current value of the number from field 1 or 2 (a digit gets added every time the field is read)
+#s6 - The current digit # (digit # of 101 is 3) of the number from field 1 or 2
+#s7 - none
+
+####### These two syscalls open the database
+li $v0, 13      	# Prepare to open file.
+la $a0, datafile	#Address of the file we want to open
+li $a1, 0			#unused, but this is here just to be safe
+li $a2, 0			#same thing
+syscall      		# Puts the file descriptor in $v0
+
+move $a0, $v0	#The output of the first syscall is the input to the next
+
+li $v0, 14    		#Prepares file to be read
+la $a1, databuffer  #Specifies buffer
+li $a2, 0xFF00      #Specifies length
+syscall      		# Reads it.
+
+add $v0, $v0, $a1	#v0 is the end
+
+li $s6, 0
+
+la $s3, currentauthor
+
+#This section loads the database a byte at a time because it is much smaller and the performance hit wouldn't be as large
+
+load_char:
+addi $v0, $v0, -1		#Note that the file is loaded from end to start to make calculations simpler
+blt $v0, $a1, finish
+
+lb $s1, 0($v0)			#Load the current byte in
+
+addi $t0, $zero, 0x2E	#period character
+beq $s1, $t0, decimal
+addi $t0, $zero, 0x2C	#comma character
+beq $s1, $t0, comma
+addi $t0, $zero, 0x0A	#newline ascii value
+beq $s1, $t0, newline
+addi $t0, $zero, 0x3A	#ascii value directly below 0
+blt $s1, $t0, check_lower_bound
+j letter
+
+check_lower_bound:
+addi $t0, $zero, 0x2F	#ascii value directly above 9
+bgt $s1, $t0, number	#if it's within the bounds we know it's a number
+j letter				#due to this being the last test we can assume it's a letter if it doesn't pass
+
+##################
+
+comma:
+addi $s4, $s4, 1		#field number
+li $t1, 1
+li $t2, 2
+beq $s4, $t1, calculate_awps
+beq $s4, $t2, calculate_acpw
+
+
+##################
+
+decimal:
+add $s2, $s6, $zero		#Saves the current digit number (represents digits to right of decimal)
+j load_char
+
+##################
+
+number:
+li $t1, 1
+li $t2, 10
+addi $s1, -0x30 		#subtract 0x30 to convert from ascii to actual value
+add $t0, $zero, $s6
+jal exp10				#exp10 returns t1 = 10^t0, t1 must be initialized as 1, t2 must be initialized as 10
+mult $s1, $t1
+mflo $s1				
+
+add $s5, $s5, $s1		#add N*10^n where N is the digit loaded and n is the number of digits so far (starting with 0)
+addi $s6, $s6, 1		#Add one to the amount of digits in this number
+
+##################
+
+letter:
+sb $s1, 0($s3)			#save character of the authors name to memory
+addi $s3, $s3, 1		#store the next value one up
+j load_char
+
+##################
+
+newline:
+li $s4, 0				#reset s4
+li $s5, 0				#reset s5
+la $t0, currentauthor	#reset the write address of the authors name to the original position.
+j clear_current
+
+################## ABOVE NEEDS TO FIXED, LINK WITH AUTHORSHIP TEST SOMEHOW
+
+authorship_test:
+sub.s $f29, $f29, $f4
+sub.s $f28, $f28, $f5
+mul.s $f28, $f29, $f28
+c.lt.s $f28, $f15
+bc1t make_positive
+number_is_positive:
+
+li $t0, 100
+li $t1, 1
+li $t2, 2
+mtc1 $t1, $f8
+cvt.s.w $f8, $f8
+mtc1 $t2, $f9
+cvt.s.w $f9, $f9
+div.s $f8, $f8, $f9			#f8 = 1/2
+add.s $f10, $f10, $f8		#f10 = 1/2, first guess
+jal calculate_root			#returns f10 = square root of f28
+
+j finish
+
+##################
+
+clear_current:
+sb $zero, 0($s3)
+addi $s3, $s3, -1
+beq $s3, $t0, load_char
+j clear_current
+
+##################
+
+exp10:
+addi $t0, $t0, -1
+blt $t0, $zero, return
+mult $t1, $t2
+mflo $t1
+j exp10
+
+##################
+
+calculate_awps:
+li $t1, 1
+li $t2, 10
+add $t0, $s2, $zero
+jal exp10
+
+mtc1 $s5, $f31
+cvt.s.w $f31, $f31
+mtc1 $t1, $f30
+cvt.s.w $f30, $f30
+
+div.s $f29, $f31, $f30
+li $s5, 0
+li $s6, 0
+
+j load_char
+
+##################
+
+calculate_acpw:
+li $t1, 1
+li $t2, 10
+add $t0, $s2, $zero
+jal exp10
+
+mtc1 $s5, $f31
+cvt.s.w $f31, $f31
+mtc1 $t1, $f30
+cvt.s.w $f30, $f30
+
+div.s $f28, $f31, $f30
+
+li $s5, 0
+li $s6, 0
+j authorship_test
+
+j load_char
+
+make_positive:
+add.s $f26, $f26, $f28
+sub.s $f28, $f28, $f26
+sub.s $f28, $f28, $f26
+j number_is_positive
+
+return:
+jr $ra
+
+calculate_root:
+div.s $f11, $f28, $f10		#f11 = S/xn
+add.s $f11, $f11, $f10		#f11 = S/xn + xn
+mul.s $f10, $f8, $f11		#f10 = 1/2(S/xn + xn)
+addi $t0, $t0, -1
+blt $t0, $zero, return
+j calculate_root
+
+finish:
+nop
+
+
+
+
+
+
+
+
+
+
+
+
